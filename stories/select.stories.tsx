4ec694644a0118fa72b0fded6c8b30bab26800ac
Gokul Raam <gokulraam.sd@skava.com>
2021-07-29 15:50:19 +0530

    add storybook for development

---
+++ stories/select.stories.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import ReactHookSelect from '../package/react-hook-select/src/ReactHookSelect';
+import '../package/react-hook-select/src/styles.scss';
+
+export default {
+  title: 'Example/Select',
+  component: ReactHookSelect,
+};
+
+const options = [
+  { label: "a", value: "a" },
+  { label: "b", value: "b" },
+  { label: "c label", value: "c" },
+  { label: "d label", value: "d" },
+]
+
+const Template = (args) => <ReactHookSelect {...args} />;
+
+export const SingleSelection = Template.bind({});
+
+export const SingleSelectionWithSearch = Template.bind({});
+
+export const MultipleSelection = Template.bind({});
+
+export const MultipleSelectionWithSearch = Template.bind({});
+
+export const MultipleSelectionWithChipView = Template.bind({});
+
+export const WithOptGroup = Template.bind({});
+
+export const ControlledMultipleSelection = () => {
+  const [value, setValue] = useState([])
+
+  return <ReactHookSelect {...{
+    label: "select",
+    placeholder: "choose option",
+    value,
+    getValue: (value) => setValue(value),
+    options,
+    enableMultiple: true,
+    chipView: false
+  }} />;
+}
+
+SingleSelection.args = {
+  label: "select",
+  placeholder: "choose option",
+  defaultValue: [],
+  options
+};
+
+SingleSelectionWithSearch.args = {
+  label: "select",
+  placeholder: "choose option",
+  defaultValue: [],
+  options,
+  enableSearch: true
+};
+
+MultipleSelection.args = {
+  label: "select",
+  placeholder: "choose option",
+  defaultValue: [],
+  options,
+  enableMultiple: true,
+  chipView: false
+};
+
+
+MultipleSelectionWithSearch.args = {
+  label: "select",
+  placeholder: "choose option",
+  defaultValue: [],
+  options,
+  enableMultiple: true,
+  chipView: false,
+  enableSearch: true
+};
+
+MultipleSelectionWithChipView.args = {
+  label: "select",
+  placeholder: "choose option",
+  defaultValue: [],
+  options,
+  enableMultiple: true,
+  enableSearch: true
+};
+
+WithOptGroup.args = {
+  label: "select",
+  placeholder: "choose option",
+  defaultValue: [],
+  options: [
+    { label: "Group A", value: "Group A", group: true },
+    { label: "a", value: "a" },
+    { label: "Group B", value: "Group B", group: true },
+    { label: "b", value: "b" },
+  ]
+};
